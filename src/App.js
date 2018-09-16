28f21f06bf9154689f4a29721b5c25020ed335bd
PhilippSchweizer <ph.m.schweizer@outlook.com>
2018-09-16 12:22:15 +0200

    create tabula rasa for approach with different npm package: react-google-maps

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { MapContainer } from './MapContainer';
 
 class App extends Component {
   render() {
-    return (
-      <MapContainer />
-    );
   }
 }
 
