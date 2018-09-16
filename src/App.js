db0ddc1e26f587f0a6f838745490c5ed51643a00
PhilippSchweizer <ph.m.schweizer@outlook.com>
2018-09-16 14:23:02 +0200

    build simple Map Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Map from './components/Map.js'
 
 class App extends Component {
   render() {
+    return(
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='Neighborhood Map' />
+          <h1 className='App-title'>Welcome to React</h1>
+        </header>
+        <Map />
+      </div>
+    )
   }
 }
 
