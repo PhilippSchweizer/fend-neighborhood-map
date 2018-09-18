058b6e25b5091b06903d9f881b2352d321960140
PhilippSchweizer <ph.m.schweizer@outlook.com>
2018-09-19 00:14:18 +0200

    follow sort tutorial on Medium https://itnext.io/google-maps-react-makes-adding-google-maps-api-to-a-react-app-a-breeze-effb7b89e54

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,7 +1,30 @@
 import React, { Component } from 'react'
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
-export class MapContainer extends Component {
+class MapContainer extends Component {
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {}
+  }
+
+  onMarkerClick = (props, marker, e) => {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    })
+  }
+
+  onMapClick = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+    }
+  }
+
   render() {
     const style = {
       width: '100vw',
@@ -10,19 +33,22 @@ export class MapContainer extends Component {
 
     return (
       <Map
-        google={this.props.google}
-        style={style}
-        zoom={14}>
+        item
+        xs = { 12 }
+        google = {this.props.google}
+        style = {style}
+        zoom = {14} >
 
-        <Marker onClick={this.onMarkerClick}
-                name={'Current location'} />
+        <Marker
+          onClick={this.onMarkerClick}
+          name={'Current location'} />
 
         {/* <InfoWindow onClose={this.onInfoWindowClose}>
             <div>
               {<h1>{this.state.selectedPlace.name}</h1>}
             </div>
         </InfoWindow> */}
-      </Map>
+      <Map>
     );
   }
 }
