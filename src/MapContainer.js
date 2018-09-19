1d14e2460ee9d50f56aad57a3bcd3454e93a58bd
PhilippSchweizer <ph.m.schweizer@outlook.com>
2018-09-19 23:24:21 +0200

    fix: GoogleApiWrapper instead of GoogleApiContainer

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,58 +1,31 @@
 import React, { Component } from 'react'
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 
-class MapContainer extends Component {
-  state = {
-    showingInfoWindow: false,
-    activeMarker: {},
-    selectedPlace: {}
-  }
-
-  onMarkerClick = (props, marker, e) => {
-    this.setState({
-      selectedPlace: props,
-      activeMarker: marker,
-      showingInfoWindow: true
-    })
-  }
-
-  onMapClick = (props) => {
-    if (this.state.showingInfoWindow) {
-      this.setState({
-        showingInfoWindow: false,
-        activeMarker: null
-      })
-    }
-  }
-
+export class MapContainer extends Component {
   render() {
     const style = {
       width: '100vw',
-      height: '100vh'
+      height: '100hv'
+    }
+    if (!this.props.loaded) {
+      return (
+        <div>Loading...</div>
+      )
     }
 
     return (
-      <Map
-        item
-        xs = { 12 }
-        google = {this.props.google}
-        style = {style}
-        zoom = {14} >
-
-        <Marker
-          onClick={this.onMarkerClick}
-          name={'Current location'} />
-
-        {/* <InfoWindow onClose={this.onInfoWindowClose}>
-            <div>
-              {<h1>{this.state.selectedPlace.name}</h1>}
-            </div>
-        </InfoWindow> */}
-      <Map>
-    );
+      <div style = {style}>
+        <Map
+          google = {this.props.google}
+          zoom = { 14 }>
+        </Map>
+      </div>
+    )
   }
 }
 
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyDYDdcgbC_9LKXiiTpr_Ld8MLXPbJADvTc'
+  apiKey: ('AIzaSyDYDdcgbC_9LKXiiTpr_Ld8MLXPbJADvTc')
 })(MapContainer)
+
+
